3e9c02db5535604032aadc1cc854675de91cbcd2
Thibault JUNIN <tjunin@scaleway.com>
2022-09-09 14:47:05 +0200

    add nested module
    Signed-off-by: Thibault JUNIN <tjunin@scaleway.com>

---
+++ src/nested_module/app1.py
@@ -0,0 +1,14 @@
+from serverless import App
+
+app = App("helloworld")
+
+
+@app.func(url="/")
+def hello_world(event: dict, context: dict):
+    """handle a request to the function
+        Args:
+            event (dict): request params
+            context (dict): function call metadata
+    """
+
+    return "Hello World!"
