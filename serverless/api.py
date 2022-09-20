ebbb32d9ebf4425a7e0ae5b3d43f53245a8959fd
Thibault JUNIN <tjunin@scaleway.com>
2022-09-20 14:14:10 +0200

    start of cli setup and api client
    Signed-off-by: Thibault JUNIN <tjunin@scaleway.com>

---
+++ serverless/api.py
@@ -0,0 +1,166 @@
+import requests
+
+API_BASE = "https://api.scaleway.com/functions/v1beta1"
+
+
+class Api:
+    def __init__(self, region: str, secret_key: str):
+        self.secret_key = secret_key
+        self.region = region
+
+    def list_namespaces(self):
+        req = requests.get(
+            f"{API_BASE}/regions/{self.region}/namespaces",
+            headers={"X-Auth-Token": self.secret_key},
+        )
+
+        if req.status_code != 200:
+            return []
+
+        return req.json()["namespaces"]
+
+    def create_namespace(
+        self,
+        name: str,
+        project_id: str,
+        env: dict = None,
+        description: str = None,
+        secrets: dict = None,
+    ):
+        req = requests.post(
+            f"{API_BASE}/regions/{self.region}/namespaces",
+            headers={"X-Auth-Token": self.secret_key},
+            json={
+                "name": name,
+                "environment_variables": env,
+                "project_id": project_id,
+                "description": description,
+                "secret_environment_variables": self.to_secret_list(secrets),
+            },
+        )
+
+        if req.status_code != 200:
+            return []
+
+        return req.json()
+
+    def to_secret_list(self, secrets: dict) -> list:
+        secrets_list = []
+
+        if secrets is not None:
+            for k, v in secrets.items():
+                secrets_list.append({"key": k, "value": v})
+
+        return secrets_list
+
+    def create_function(
+        self,
+        name: str,
+        namespace_id: str,
+        runtime: str,
+        handler: str,
+        privacy: str = "unknown_privacy",
+        env: dict = None,
+        min_scale: int = None,
+        max_scale: int = None,
+        memory_limit: int = None,
+        timeout: str = None,
+        description: str = None,
+        secrets: dict = None,
+    ):
+        req = requests.post(
+            f"{API_BASE}/regions/{self.region}/functions",
+            headers={"X-Auth-Token": self.secret_key},
+            json={
+                "name": name,
+                "namespace_id": namespace_id,
+                "environment_variables": env,
+                "min_scale": min_scale,
+                "max_scale": max_scale,
+                "runtime": runtime,
+                "memory_limit": memory_limit,
+                "timeout": timeout,
+                "handler": handler,
+                "privacy": privacy,
+                "description": description,
+                "secret_environment_variables": self.to_secret_list(secrets),
+            },
+        )
+
+        if req.status_code != 200:
+            return []
+
+        return req.json()
+
+    def list_functions(self, namespace_id: str):
+        req = requests.get(
+            f"{API_BASE}/regions/{self.region}/functions?namespace_id={namespace_id}",
+            headers={"X-Auth-Token": self.secret_key},
+        )
+
+        if req.status_code != 200:
+            return []
+
+        return req.json()["functions"]
+
+    def upload_function(self, function_id: str, content_length: int):
+        req = requests.get(
+            f"{API_BASE}/regions/{self.region}/functions/{function_id}/upload-url?content_length={str(content_length)}",
+            headers={"X-Auth-Token": self.secret_key},
+        )
+
+        if req.status_code != 200:
+            return None
+
+        return req.json()["url"]
+
+    def deploy_function(self, function_id: str):
+        req = requests.post(
+            f"{API_BASE}/regions/{self.region}/functions/{function_id}/deploy",
+            headers={"X-Auth-Token": self.secret_key},
+        )
+
+        return req.status_code == 200
+
+    def update_function(
+        self,
+        function_id: str,
+        runtime: str,
+        handler: str,
+        privacy: str = "unknown_privacy",
+        env: dict = None,
+        min_scale: int = None,
+        max_scale: int = None,
+        memory_limit: int = None,
+        timeout: str = None,
+        description: str = None,
+        secrets: dict = None,
+    ):
+        req = requests.patch(
+            f"{API_BASE}/regions/{self.region}/functions/{function_id}",
+            headers={"X-Auth-Token": self.secret_key},
+            json={
+                "min_scale": min_scale,
+                "max_scale": max_scale,
+                "runtime": runtime,
+                "memory_limit": memory_limit,
+                "timeout": timeout,
+                "handler": handler,
+                "privacy": privacy,
+                "description": description,
+                "secret_environment_variables": self.to_secret_list(secrets),
+            },
+        )
+
+        if req.status_code != 200:
+            return []
+
+        return req.json()
+
+    def delete_function(self, function_id: str):
+        req = requests.delete(
+            f"{API_BASE}/regions/{self.region}/functions/{function_id}",
+            headers={"X-Auth-Token": self.secret_key},
+        )
+
+        return req.status_code == 200
--- serverless/cli.py
+++ serverless/cli.py
@@ -16,7 +16,34 @@ def cli():
     pass
 
 
-@cli.command()
+@cli.command(help="Deploy your functions to scaleway")
+@click.option(
+    "--file",
+    "-f",
+    "file",
+    default="./tests/app.py",
+    show_default=True,
+    help="Select the file containing your functions handlers",
+)
+def deploy(file):
+    app_instance = get_app_instance(file)
+
+    """
+    List user's namespaces.
+     If a namespace exist in the same project and region, use this one
+     otherwise, create a new namespace
+     
+    List functions in the namespace
+        if a function exists with the same name, update it.
+        otherwise create a new one
+    
+    delete functions that are no longer present in the project
+    
+    update the namespace if it was already existing with the new configuration
+    """
+
+
+@cli.command(help="Generate the configuration file for your functions")
 @click.option(
     "--target",
     "-t",
@@ -43,6 +70,20 @@ def cli():
     help="Select where to save the generated configuration file",
 )
 def generate(file, target, save):
+    app_instance = get_app_instance(file)  # Get the serverless App instance
+
+    click.echo(f"Generating configuration for target: {target}")
+
+    if target == "serverless":
+        serverless_framework_generator = ServerlessFrameworkGenerator(app_instance)
+        serverless_framework_generator.write(save)
+
+    click.echo(
+        click.style(f"Done! Generated configuration file saved in {save}", fg="green")
+    )
+
+
+def get_app_instance(file: str) -> Serverless:
     abs_file = os.path.abspath(file)
 
     if not os.path.exists(abs_file):
@@ -83,15 +124,7 @@ def generate(file, target, save):
         )
         raise ImportError
 
-    click.echo(f"Generating configuration for target: {target}")
-
-    if target == "serverless":
-        serverless_framework_generator = ServerlessFrameworkGenerator(app_instance)
-        serverless_framework_generator.write(save)
-
-    click.echo(
-        click.style(f"Done! Generated configuration file saved in {save}", fg="green")
-    )
+    return app_instance
 
 
 def main():
