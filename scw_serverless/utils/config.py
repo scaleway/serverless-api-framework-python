bc7dccd6c20278444c5c64dddb557dbb66971025
Andy Méry <amery@scaleway.com>
2022-11-16 16:56:11 +0100

    fix: switch to pyproject.toml

--- scw_serverless/utils/config.py
+++ scw_serverless/utils/config.py
@@ -21,9 +21,7 @@ class Config:
         config_path = Path(DEFAULT_CONFIG_PATH).expanduser()
 
         if not config_path.exists():
-            raise RuntimeError(
-                f"could not read config file at {config_path.absolute()}"
-            )
+            return self
 
         with open(config_path, mode="rt", encoding="utf-8") as file:
             config = yaml.safe_load(file)
