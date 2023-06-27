afa2dfc3cd8b6c420e1ab27f2c1894675f9854d3
Andy Méry <amery@scaleway.com>
2023-06-27 14:49:19 +0200

    refactor: remove old gateway constants

--- tests/constants.py
+++ tests/constants.py
@@ -9,13 +9,6 @@ SCALEWAY_FNC_API_URL = SCALEWAY_API_URL + f"functions/v1beta1/regions/{DEFAULT_R
 
 COLD_START_TIMEOUT = 20
 
-GATEWAY_HOST = os.getenv("GATEWAY_HOST")
-GATEWAY_S3_REGION = os.getenv("S3_REGION", str(DEFAULT_REGION))
-GATEWAY_S3_BUCKET_ENDPOINT = os.getenv(
-    "S3_ENDPOINT", f"https://s3.{DEFAULT_REGION}.scw.cloud"
-)
-GATEWAY_S3_BUCKET_NAME = os.getenv("GATEWAY_S3_BUCKET_NAME")
-
 TESTS_DIR = os.path.realpath(os.path.dirname(__file__))
 PROJECT_DIR = Path(TESTS_DIR).parent
 
